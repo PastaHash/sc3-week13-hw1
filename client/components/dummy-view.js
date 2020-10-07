3558214f96dab011bfb9874bf60cad14c1650a1c
Vitaly K <43697710+shut56@users.noreply.github.com>
2020-10-07 18:49:32 +0500

    Initial commit

---
+++ client/components/dummy-view.js
@@ -0,0 +1,24 @@
+import React from 'react'
+import { Switch, Route } from 'react-router-dom'
+import Head from './head'
+
+import Main from './main'
+import RepoList from './repolist'
+import Text from './text'
+
+const Dummy = () => {
+  return (
+    <div>
+      <Head title="Hello" />
+      <Switch>
+        <Route exact path="/" component={() => <Main />} />
+        <Route exact path="/:userName" component={() => <RepoList />} />
+        <Route exact path="/:userName/:repositoryName" component={() => <Text />} />
+      </Switch>
+    </div>
+  )
+}
+
+Dummy.propTypes = {}
+
+export default Dummy
---
+++ client/components/header.js
@@ -0,0 +1,24 @@
+import React from 'react'
+import { Link, useParams } from 'react-router-dom'
+
+const Header = (props) => {
+  const { userName, repositoryName } = useParams()
+
+  return (
+    <nav className="flex bg-indigo-800 items-center justify-between flex-wrap p-6 text-white font-bold">
+      <div className="flex flex-shrink-0 mr-6" id="repository-name">
+        {userName}
+      </div>
+      {repositoryName && (
+        <Link id="go-repository-list" to={`/${userName}`}>
+          Go repository list
+        </Link>
+      )}
+      <Link id="go-back" to="/">
+        Go back
+      </Link>
+    </nav>
+  )
+}
+
+export default React.memo(Header)
